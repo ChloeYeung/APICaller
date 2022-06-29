a61da6588d0e42900e93525189f34d48e6ca9774
ChloeYeung <ycyching0117@gmail.com>
2022-06-29 20:39:09 +0800

    1st

---
+++ script.js
@@ -0,0 +1,32 @@
+let JOKEAPI = document.getElementById("JOKEAPI");
+let repsonseText = document.getElementsByClassName(apiResponse)[0];
+JOKEAPI.addEventListener("click", function (){
+    document.getElementsByClassName(apiSource)[0].innerHTML = "JOKE API";
+    fetch ("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=political")
+    .then((response) => response.json())
+    .then((data)=> (responseText.innerHTML = data.joke));
+})
+
+
+let KanyeAPI = document.getElementById("KanyeAPI");
+KanyeAPI.addEventListener('click', function (){
+    document.getElementsByClassName(apiSource)[0].innerHTML = "Kanye API";
+    fetch ("https://api.kanye.rest/")
+    .then((response) => response.json())
+    .then((data)=> (responseText.innerHTML = data.quote));
+});
+
+let UniAPI = document.getElementById("UniAPI");
+UniAPI.addEventListener ('click', function (){
+    document.getElementsByClassName(apiSource)[0].innerHTML = "Universities API";
+    fetch ("http://universities.hipolabs.com/search?country=Japan")
+    .then((response) => response.json())
+    .then((data)=>  {
+        responseText.innerHTML = " ";
+        data.map((obj)=> {
+            let p = document.createElement("p");
+            p.innerHTML = obj.name;
+            responseText.append(p); //add something inside
+
+        });
+}); 
